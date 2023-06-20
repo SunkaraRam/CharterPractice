6a01e6b178fe1af9281294314070c5952092648f
Sunkararam <sunkararam1166@gmail.com>
2023-06-20 15:02:50 +0530

    changes from us person

--- src/test/java/DemoProgram2.java
+++ src/test/java/DemoProgram2.java
@@ -3,6 +3,8 @@ public class DemoProgram2 {
 	
 	public static void main(String[] args) {
 		System.out.println("This is demo2 program");
+		System.out.println("This is demo2 pragram line1");
+		System.out.println("This is demo2 pragram line2");
 	}
 
 }
